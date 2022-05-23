6ed689ac5c3723e1925290dc1ca79fa88fe03633
dentrunov <sgema@rambler.ru>
2022-05-23 07:58:40 +0300

    d

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from flask_moment import Moment
+
+app = Flask(__name__, static_folder="static")
+moment = Moment(app)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/test/')
+def test():
+    return render_template('test.html')
+
+
+
+if __name__ == '__main__':
+    #не забыть снять дебаг
+    app.run(debug=True)
